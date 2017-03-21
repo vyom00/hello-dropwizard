64c03294a09697b8e0ba3649f97a9abed7a19f06
Haroon Rasheed <haroon.rasheed@dnata.com>
2017-03-21 23:01:37 +0400

    Initial commit

---
+++ src/main/java/com/pragtech/HelloWorldApplication.java
@@ -0,0 +1,38 @@
+package com.pragtech;
+
+import com.pragtech.config.HelloWorldConfiguration;
+import com.pragtech.health.TemplateHealthCheck;
+import com.pragtech.resources.HelloWorldResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
+    public static void main(String[] args) throws Exception {
+        new HelloWorldApplication().run(args);
+    }
+
+    @Override
+    public String getName() {
+        return "hello-world";
+    }
+
+    @Override
+    public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
+        // nothing to do yet
+    }
+
+    @Override
+    public void run(HelloWorldConfiguration configuration,
+                    Environment environment) {
+        final HelloWorldResource resource = new HelloWorldResource(
+                configuration.getTemplate(),
+                configuration.getDefaultName()
+        );
+        final TemplateHealthCheck healthCheck =
+                new TemplateHealthCheck(configuration.getTemplate());
+        environment.healthChecks().register("template", healthCheck);
+        environment.jersey().register(resource);
+    }
+
+}
